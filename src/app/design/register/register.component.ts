c560d9e0a67c987b8f5222e830481f4bd999493a
sviniba <sviniba@msservicesinc.com>
2023-12-05 19:34:55 +0530

    design changes

--- src/app/design/register/register.component.ts
+++ src/app/design/register/register.component.ts
@@ -1,23 +1,21 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent {
-
   isVisible = true;
 
-  saveType() {
-    
-      this.isVisible = false;
-    
-  }
+    saveType() {      
+        this.isVisible = false;      
+    }
 
-  close(){
-    this.isVisible=false;
-  }
+    close() {
+      this.isVisible=false;
+    }
 
   
 
