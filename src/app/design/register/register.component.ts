013dd2afe30a8f0df249919bb236699013dc62fe
sviniba <sviniba@msservicesinc.com>
2023-12-26 19:07:14 +0530

    design changes

--- src/app/design/register/register.component.ts
+++ src/app/design/register/register.component.ts
@@ -7,18 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent {
-  isVisible = true;
+
   isChecked1: boolean = true; 
   isChecked2: boolean = false;
+  isVisible: boolean = false  
 
-    saveType() {      
-        this.isVisible = false;      
-    }
+    // /saveType() {      
+    //     this.isVisible = false;      
+    // }
 
-    close() {
-      this.isVisible=false;
-    }
+    // close() {
+    //   this.isVisible=false;
+    // }
 
   
+    togglePasswordVisibility() {
+      this.isVisible = !this.isVisible;
+  }
 
 }
