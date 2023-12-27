0b2c60d682de05212a8d19b6a5d9d634caef4486
sviniba <sviniba@msservicesinc.com>
2023-12-27 15:22:12 +0530

    students componet validation careated

--- src/app/design/register/register.component.ts
+++ src/app/design/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { UntypedFormControl, UntypedFormGroup, Validators } from '@angular/forms';
 
 
 @Component({
@@ -8,8 +9,8 @@ import { Component } from '@angular/core';
 })
 export class RegisterComponent {
 
-  isChecked1: boolean = true; 
-  isChecked2: boolean = false;
+  // isChecked1: boolean = true; 
+  // isChecked2: boolean = false;
   isVisible: boolean = false  
 
     // /saveType() {      
@@ -20,7 +21,84 @@ export class RegisterComponent {
     //   this.isVisible=false;
     // }
 
+    signup = new UntypedFormGroup({
+      firstName : new UntypedFormControl('',[Validators.required,Validators.nullValidator]),
+      lastName : new UntypedFormControl('',[Validators.required,Validators.nullValidator]),
+      email : new UntypedFormControl('',[Validators.required,Validators.nullValidator]),
+      passWord : new UntypedFormControl('',[Validators.required,Validators.nullValidator]),
+      confirmPassword : new UntypedFormControl('',[Validators.required,Validators.nullValidator]),
+    })
+
+
+     public signupError= {
+      firstName: '',
+      lastName: '',
+      email: '',
+      passWord: '',
+      confirmPassword: '',
+    }
+
+    onSubmit(){
+      this.formValidation("Active");
+    }
+
+    formValidation(status:String){
+      this.signupError.firstName="";
+      this.signupError.lastName="";
+      this.signupError.email="";
+      this.signupError.passWord="";
+      this.signupError.confirmPassword="";
+
+
+      let hasError=false;
+
+      if(this.signup.get('firstName')?.invalid){
+        this.signupError.firstName="Enter First Name";
+        hasError=true;
+      }
+
+      if(this.signup.get('lastName')?.invalid){
+        this.signupError.lastName="Enter Last Name";
+        hasError=true;
+      }
+
+      if(this.signup.get('email')?.invalid){
+        this.signupError.email="Enter E-Mail Address";
+        hasError=true;
+      }
+
+      if(this.signup.get('passWord')?.invalid){
+        this.signupError.passWord="Enter new Password";
+        hasError=true;
+      }
+      
+      if (this.signup.get('confirmPassword')?.invalid) {
+        const passwordControl = this.signup.get('passWord');
+        const confirmPasswordControl = this.signup.get('confirmPassword'); 
+        console.log("password:");       
+        if (passwordControl && confirmPasswordControl && passwordControl.value !== confirmPasswordControl.value) {
+          confirmPasswordControl.setErrors({ passwordMismatch: true });
+        }else{
+          this.signupError.confirmPassword = "Password do not match";
+          hasError = true;
+        }
+      }
+        
+
+      if(!hasError) {
+        this.saveSignup(status);
+    }
+    }
+
+    
   
+
+    saveSignup(status : String){
+      console.log("saved")
+
+    }
+
+
     togglePasswordVisibility() {
       this.isVisible = !this.isVisible;
   }
--- src/app/design/students/students.component.ts
+++ src/app/design/students/students.component.ts
@@ -14,7 +14,7 @@ export class StudentsComponent {
 
   }
 
-  epkitchendetails = new UntypedFormGroup({
+  students = new UntypedFormGroup({
 
     name: new UntypedFormControl('', [Validators.required, Validators.nullValidator]),
     department: new UntypedFormControl('', [Validators.required, Validators.nullValidator]),
@@ -22,5 +22,65 @@ export class StudentsComponent {
     college: new UntypedFormControl('', [Validators.required, Validators.nullValidator]),
     year: new UntypedFormControl('', [Validators.required, Validators.nullValidator]),
   })
+
+  public studentError = {
+    name: '',
+    department: '',
+    email: '',
+    college: '',
+    year: '',
+  }
+
+  formValidation(status:String){
+    this.studentError.name="";
+    this.studentError.department="";
+    this.studentError.email="";
+    this.studentError.college="";
+    this.studentError.year="";
+
+
+    let hasError=false;
+
+    if(this.students.get('name')?.invalid){
+      this.studentError.name="Enter Student Name";
+      hasError=true;
+    }
+
+    if(this.students.get('department')?.invalid){
+      this.studentError.department="Enter Department Name";
+      hasError=true;
+    }
+
+    if(this.students.get('email')?.invalid){
+      this.studentError.email="Enter E-Mail Address";
+      hasError=true;
+    }
+
+    if(this.students.get('college')?.invalid){
+      this.studentError.college="Enter College Name";
+      hasError=true;
+    }
+
+    if(this.students.get('year')?.invalid){
+      this.studentError.year="Enter Education Year";
+      hasError=true;
+    }
+
+    if(!hasError){
+      this.saveStudentsDetails(status)
+    }
+
+
+  }
+
+  onSubmit(){
+    this.formValidation("Active")
+  }
+
+
+  saveStudentsDetails(status : String){
+    console.log("SAVE DETAILS");
+  }
+
 
 }
